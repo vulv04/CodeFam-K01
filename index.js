02ce861b2bd6abfa929e967ba48d793bab073d84
vulv04 <vulv04.it@gmail.com>
2025-06-16 16:22:06 +0700

    donee

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectDB from "./src/common/configs/connectDB.js";
-import { PORT, HOST } from "./src/common/configs/enviroments.js";
+import { PORT} from "./src/common/configs/enviroments.js";
 import router from "./src/routes/index.js";
 import errorHandler from "./src/common/middlewares/errorHandle.js";
 import setupSwagger from "./src/common/configs/swagger-config.js";
@@ -16,7 +16,7 @@ app.use("/api", router);
 app.use(errorHandler);
 setupSwagger(app);
 
-app.listen(PORT, HOST, () => {
-	console.log(`Server running at http://${HOST}:${PORT}/`);
-	console.log(`Swagger Docs available at ${HOST}:${PORT}/api-docs`);
+app.listen(PORT, () => {
+	console.log(`Server running at http://${PORT}/`);
+	console.log(`Swagger Docs available at${PORT}/api-docs`);
 });
--- src/common/configs/enviroments.js
+++ src/common/configs/enviroments.js
@@ -1,15 +1,9 @@
 import dotenv from "dotenv";
 
 dotenv.config({
-	path: ".env", // Load biến môi trường từ file .env.{NODE_ENV}
-	debug: true, // Hiển thị thông báo debug
-	encoding: "utf8", // Encoding của file .env
-	silent: true, // Không hiển thị thông báo lỗi nếu file .env không tồn tại
-	defaults: true, // Load các biến môi trường mặc định từ file .env.defaults
-	ignoreProcessEnv: true, // Không ghi đè các biến môi trường đã tồn tại trong process.env
-	expand: true, // Mở rộng các biến môi trường có giá trị là biến môi trường khác
-	assignToProcessEnv: true, // Gán các biến môi trường vào process.env
-	overrideProcessEnv: true, // Ghi đè các biến môi trường đã tồn tại trong process.env
+  path: ".env", 
+  debug: true,
+  encoding: "utf8",
 });
 
 export const { DB_URI, HOST, PORT } = process.env;
